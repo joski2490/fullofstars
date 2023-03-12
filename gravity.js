5a9638264dca40e0a726a1129f80d500b97b7ff9
joski2490 <68449917+joski2490@users.noreply.github.com>
2023-03-12 10:48:06 -0500

    Update gravity.js

--- gravity.js
+++ gravity.js
@@ -323,7 +323,7 @@ fullofstars.createGravitySystem = function(particleCount, typicalMass, makeBlack
     var BLACK_HOLE_MASS = fullofstars.TYPICAL_STAR_MASS * 10000;
 
     for (var p = 0; p < particleCount; p++) {
-        var angle = Math.cosh(Math.sqrt((Math.random() * 10)+(Math.random()*10)))*10;//200 + Math.PI * 2 * Math.random();
+        var angle = Math.atan(Math.cosh(Math.sqrt((Math.random() * 10)+(Math.random()*10)))*10)*Math.Pi/2;//200 + Math.PI * 2 * Math.random();
 
         // This creates density variations angularly
         angle += /*0.10*/ (Math.cbrt(Math.random())/3) * Math.sin(angle * Math.PI*2);
